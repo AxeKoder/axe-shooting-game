f207aa9687dabd5acfe4e5330025a1e23349322d
axekoder <soularrow@naver.com>
2024-02-19 00:12:44 +0900

    - 구글 애드몹 추가 - pod 추가

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ShootingGame' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ShootingGame
+  pod 'Google-Mobile-Ads-SDK'
+
+end
--- ShootingGame/GameViewController.swift
+++ ShootingGame/GameViewController.swift
@@ -8,11 +8,29 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import GoogleMobileAds
 
 class GameViewController: UIViewController {
+    
+    lazy var adBannerView: GADBannerView = {
+        let adBannerView = GADBannerView(adSize: GADPortraitAnchoredAdaptiveBannerAdSizeWithWidth(view.frame.width))
+        adBannerView.frame.origin = CGPoint(x: 0, y: view.frame.size.height - adBannerView.frame.height)
+        adBannerView.frame.size = CGSize(width: view.frame.width, height: adBannerView.frame.height)
+        adBannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        adBannerView.delegate = self
+        adBannerView.rootViewController = self
+        return adBannerView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let uuidString = UIDevice.current.identifierForVendor?.uuidString
+        if let uuidString {
+            GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [uuidString]
+        }
+        let adMobRequest = GADRequest()
+        adBannerView.load(adMobRequest)
         
         if let view = self.view as! SKView? {
             let scene = MenuScene(size: view.bounds.size)
@@ -36,5 +54,16 @@ class GameViewController: UIViewController {
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+}
+
+extension GameViewController: GADBannerViewDelegate {
+    func bannerViewDidReceiveAd(_ bannerView: GADBannerView) {
+        view.addSubview(adBannerView)
+    }
+    
+    func bannerView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: Error) {
+        print("Fail to receive ads")
+        print(error)
     }
 }
